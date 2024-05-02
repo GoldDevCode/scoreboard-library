9d651d4cbb89f4d27cea389431c526dcbdd8aae5
GoldDevCode <swarnimb6@gmail.com>
2024-05-02 16:46:16 +0200

    Setup jdk version and testing

---
+++ src/test/java/com/sportsradar/dev/test/ScoreBoardTests.java
@@ -0,0 +1,19 @@
+package com.sportsradar.dev.test;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class ScoreBoardTests {
+    /**
+     * Initial test to check if the test framework is working
+     */
+    @Test
+    @DisplayName("Test if the test framework is working")
+    public void testSetupOK() {
+        assertTrue(true);
+    }
+}
